92f8ca0705878f1dd30476f6cabaac6205ee439d
leenalzaben <leena.zaben61@gmail.com>
2023-03-25 18:40:35 +0000

    Lab13

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE MoviedB_table (
+    title VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview VARCHAR(255),
+    release_date VARCHAR(255)
+
+);
--- server.js
+++ server.js
@@ -5,14 +5,23 @@ const express = require("express");
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const { Client } = require('pg')
+
 const MoviesData = require('./Movie Data/data.json');
 const { json } = require('express');
 const PORT = process.env.PORT;
 const apiKey = process.env.API_KEY;
+//postgres://username:password@localhost:5432/databasename
+let url =`postgres://leena:0000@localhost:5432/moviedb`;
+// const URL = process.env.url;
+const client = new Client(url)
 // notice to use all apps after declration ,where the app here is express which bulid the server
 const app = express();
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 //Routes 
 app.get("/", homeHandler);
 app.get("/favorite", favoriteHandler);
@@ -20,9 +29,10 @@ app.get('/trending', trendingHandler);
 app.get("/search", searchHandler);
 app.get("/popular", popularHandler);
 app.get("/overview", overview);
+app.post("/addMovie", addMovieHandler);
+app.get("/getMovies",getMoviesHandeler);
+///getMovies
 app.get('*', HandlerNotFoundError);
-
-
 
 // functions
 //1. Home
@@ -37,7 +47,6 @@ function favoriteHandler(req, res) {
     res.send("Welcome to Favorite Page");
 }
 //3. trending 
-
 function trendingHandler(req, res) {
     let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
     axios.get(url)
@@ -54,7 +63,7 @@ function trendingHandler(req, res) {
         })
 
 }
-
+//4.search
 function searchHandler(req, res) {
     let clientQuery = req.query.title;
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${clientQuery}&page=2`
@@ -71,9 +80,7 @@ function searchHandler(req, res) {
 
 
 }
-
-
-
+//5.popular
 function popularHandler(req, res) {
     let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
     axios.get(url)
@@ -89,7 +96,7 @@ function popularHandler(req, res) {
         })
 
 }
-
+//6. overview
 function overview(req, res) {
     const endpoint = 'https://api.themoviedb.org/3/movie/top_rated';
     const url = `${endpoint}?api_key=${apiKey}`;
@@ -110,6 +117,44 @@ function overview(req, res) {
 }
 
 
+
+// POST Method
+function addMovieHandler(req, res) {
+    // it should be save the data
+    console.log(req.body);
+    // save data inside variable use destructing way
+    let{title,poster_path,overview,release_date} = req.body;
+    let sql = `INSERT INTO MoviedB_table (title,poster_path,overview,release_date)
+    VALUES ($1,$2,$3,$4) RETURNING *; `
+    let values = [title,poster_path,overview,release_date]
+        // client.query(sql,values).then((result)=>{
+        res.status(201).send("data recived to the server successfully")
+        // res.status(201).json(values);
+        // )
+        // .catch((error) => {
+        //     console.log(error);
+        // })
+}
+// get to return all data I saved in dB
+function getMoviesHandeler(req,res){
+    let sql =`SELECT * FROM MoviedB_table;`
+    // client.query(sql);
+    client.query(sql).then((result)=>{
+        console.log(sql);
+
+        res.json(result);
+    }).catch((error)=>{
+        errorHandler(error,req,res)
+    })
+
+
+
+
+
+}
+
+
+//7. error found
 function HandlerNotFoundError(req, res) {
     res.status(404).send("Not Found");
 }
@@ -122,8 +167,6 @@ app.use(function (error, req, res, next) {
 
 
 });
-
-
 // My constructor
 function movieInfo(title, poster_path, overview) {
     this.title = title;
@@ -131,8 +174,6 @@ function movieInfo(title, poster_path, overview) {
     this.overview = overview;
 
 };
-// my constructor II(
-
 function trendMovie(id, title, release_date, poster_path, overview) {
     this.id = id;
     this.title = title;
@@ -141,8 +182,6 @@ function trendMovie(id, title, release_date, poster_path, overview) {
     this.overview = overview;
 
 }
-
-// my constructor III(search) vote
 function popular(title, overview, release_date, vote) {
     this.title = title;
     this.overview = overview;
@@ -158,11 +197,13 @@ function topShow(name, lang, overview, vote) {
 
 }
 
+client.connect().then(() => {
+    // Run server and make it listening all the time
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 
-// Run server and make it listening all the time
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+}).catch();
 
 
 
